b4456b38e75558f619eb66e25d685707ea910eaa
progand <lach@ukr.net>
2013-07-02 15:01:17 +0300

    [*] Змфни в анкеті
    Що стосується Анкети
    
    
    - Заголовок:   Заявка на оформлення кредиту
    -  Перед графою " Місто" додати графу "Область" (Також щоб бачили тільки ми)
    - "Сума" - "Бажана сума кредиту"
    
    - Внизу квадратик для галочки і текст: Я згоден.... (текст згоди підготуємо)
    
    - Кнопка "Оформити" більша і яскрава з текстом: "Замовити кредит" 
    
    
    
    - І якщо це можливо, поряд з анкетою текст: 
    
    
    
    Три простих кроки до отримання кредиту:
    
    
    1. Заповнити анкету
    2. Очікувати дзвінок. Вам передзвонить наш спеціаліст, проконсультує та надасть попереднє рішення за кредитом
    3. Прийти до найближчого до Вас відділення та отримати готівку.
    
    
    Основні умови:
    - кредит - на будь-які цілі
    - вік: від 21 до 75 років;
    - працевлаштування (офіційно або ні).
    
    
    Документи для отримання:
    - паспорт;
    - код ІПН;
    - пенсійне посвідчення для пенсіонерів;
    - довідка про доходи (за бажанням).

--- administration/protected/views/questionnaire/create_on_frontend.php
+++ administration/protected/views/questionnaire/create_on_frontend.php
@@ -3,19 +3,32 @@
 /* @var $model Questionnaire */
 ?>
 
-
+<style type="text/css">
+    input[type=submit]{
+        padding: 15px;
+        font-weight: bold;
+        font-size: 1.5em;
+        color: green;
+    }
+    #header, #footer, #mainmenu{
+        display: none !important;
+    }
+    #error-details{
+        display: none;
+    }
+</style>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript">
     jQuery(function() {
-        jQuery("#header").add("#footer").add("#mainmenu").remove();
-        jQuery("#error-details").hide();
-        jQuery("#show-error-details").click(function(){
-            jQuery("#error-details").toggle();
-            return false;
-        });
+    jQuery("#header").add("#footer").add("#mainmenu").hide();
+    jQuery("#error-details").hide();
+    jQuery("#show-error-details").click(function(){
+    jQuery("#error-details").toggle();
+    return false;
+    });
     });
 </script>
-<h1>Оформити заявку</h1>
+<h1>Заявка на оформлення кредиту</h1>
 <?php if (isset($exception) && $exception) { ?>
     <h2>
         Під час реєстрації заявки в webapp.rccf.ua сталася помилка! Заявку не зареєстровано. 
@@ -48,3 +61,25 @@
     echo $this->renderPartial('_form', array('model' => $model));
 }
 ?>
+
+<h2>Три простих кроки до отримання кредиту:</h2>
+<ol>
+    <li>Заповнити анкету</li>
+    <li>Очікувати дзвінок. Вам передзвонить наш спеціаліст, проконсультує та надасть попереднє рішення за кредитом</li>
+    <li>Прийти до найближчого до Вас відділення та отримати готівку.</li>
+</ol>
+
+<h2>Основні умови:</h2>
+<ul>
+    <li>кредит - на будь-які цілі</li>
+    <li>вік: від 21 до 75 років</li>
+    <li>працевлаштування (офіційно або ні)</li>
+</ul>
+
+<h2>Документи для отримання:</h2>
+<ul>
+    <li>паспорт</li>
+    <li>код ІПН</li>
+    <li>пенсійне посвідчення для пенсіонерів</li>
+    <li>довідка про доходи (за бажанням)</li>
+</ul>
